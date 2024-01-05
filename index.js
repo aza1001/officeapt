5c3314bb2178157c1b389618b849b84de5497287
aza1001 <azizah.zainal11@gmail.com>
2024-01-05 22:27:26 +0800

    5 push

--- index.js
+++ index.js
@@ -221,3 +221,122 @@ app.post('/register-staff', authenticateToken, async (req, res) => {
       });
   });
   
+
+  /**
+* @swagger
+* /login-staff:
+*   post:
+*     summary: Staff login
+*     tags: [Staff]
+*     requestBody:
+*       content:
+*         application/json:
+*           schema:
+*             type: object
+*             properties:
+*               username:
+*                 type: string
+*               password:
+*                 type: string
+*     responses:
+*       200:
+*         description: Staff logged in successfully
+*         content:
+*           application/json:
+*             schema:
+*               type: object
+*               properties:
+*                 token:
+*                   type: string
+*       401:
+*         description: Invalid credentials
+*       500:
+*         description: Error storing token
+*/
+
+// Staff login
+app.post('/login-staff', async (req, res) => {
+    const { username, password } = req.body;
+  
+    const staff = await staffDB.findOne({ username });
+  
+    if (!staff) {
+      return res.status(401).send('Invalid credentials');
+    }
+  
+    const passwordMatch = await bcrypt.compare(password, staff.password);
+  
+    if (!passwordMatch) {
+      return res.status(401).send('Invalid credentials');
+    }
+  
+    const token = jwt.sign({ username, role: 'staff' }, secretKey);
+    staffDB
+      .updateOne({ username }, { $set: { token } })
+      .then(() => {
+        res.status(200).json({ token });
+      })
+      .catch(() => {
+        res.status(500).send('Error storing token');
+      });
+  });
+  
+  /**
+  * @swagger
+  * /login-security:
+  *   post:
+  *     summary: Security login
+  *     tags: [Security]
+  *     requestBody:
+  *       content:
+  *         application/json:
+  *           schema:
+  *             type: object
+  *             properties:
+  *               username:
+  *                 type: string
+  *               password:
+  *                 type: string
+  *     responses:
+  *       200:
+  *         description: Security logged in successfully
+  *         content:
+  *           application/json:
+  *             schema:
+  *               type: object
+  *               properties:
+  *                 token:
+  *                   type: string
+  *       401:
+  *         description: Invalid credentials
+  *       500:
+  *         description: Error storing token
+  */
+  
+  // Security login
+  app.post('/login-security', async (req, res) => {
+    const { username, password } = req.body;
+  
+    const security = await securityDB.findOne({ username });
+  
+    if (!security) {
+      return res.status(401).send('Invalid credentials');
+    }
+  
+    const passwordMatch = await bcrypt.compare(password, security.password);
+  
+    if (!passwordMatch) {
+      return res.status(401).send('Invalid credentials');
+    }
+  
+    const token = security.token || jwt.sign({ username, role: 'security' }, secretKey);
+    securityDB
+      .updateOne({ username }, { $set: { token } })
+      .then(() => {
+        res.status(200).json({ token });
+      })
+      .catch(() => {
+        res.status(500).send('Error storing token');
+      });
+  });
+  
